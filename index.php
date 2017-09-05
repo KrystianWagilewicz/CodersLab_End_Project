2b9aa2d0626164920b9ee67d144c2727c6a1f767
KrystianWagilewicz <wagilewicz.krystian@gmail.com>
2017-09-05 20:55:47 +0200

    Implement password verify function to login script and rebuild it, some code cleanup

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
 	if (isset($_SESSION['logged']))
 	{
     $session_status = $_SESSION['logged'];
-		// header('Location: index.php');
-		// exit();
 	}
 ?>
 
--- login.php
+++ login.php
@@ -2,8 +2,7 @@
 
 	session_start();
 
-	if ((!isset($_POST['login'])) || (!isset($_POST['password'])))
-	{
+	if ( (!isset($_POST['login']) ) || (!isset($_POST['password'])) ) {
 		header('Location: index.php');
 		exit();
 	}
@@ -13,32 +12,40 @@
 		$login = $_POST['login'];
 		$password = $_POST['password'];
 
-	  $sql = 'SELECT * FROM user_account WHERE email = "'.$login.'" AND password = "'.$password.'"';
+		$verify_sql = 'SELECT password FROM user_account WHERE email="'.$login.'"';
 
-		//result of db query
-		if ($result = mysqli_query($connect, $sql))
-		{
-			//check how many records db will return
-			$user_ammount = $result->num_rows;
-			if($user_ammount>0){
-				$_SESSION['logged'] = true;
+	  //sql result
+	  $verify_result = mysqli_query($connect, $verify_sql);
+	  //associative table so now we can use table column names
+	  $verify_password = $verify_result->fetch_assoc();
 
-				//associative table so now we can use table column names
-				$user_table = $result->fetch_assoc();
+	  //place data from associative table to selected variable
+	  $hash = $verify_password['password'];
 
-				//place data from associative table to selected variable
-				$_SESSION['id'] = $user_table['id'];
-				$_SESSION['email'] = $user_table['email'];
+		if (password_verify($password, $hash)) {
+			$sql = 'SELECT id , email FROM user_account WHERE email = "'.$login.'" ';
 
-				unset($_SESSION['error']);
+			//result of db query
+			$result = mysqli_query($connect, $sql);
 
-				//clear
-				$result->free_result();
-				header('Location: index.php');
-			} else {
-				$_SESSION['error'] = '<span style="color:red">Wrong login or password!</span>';
-				header('Location: index.php');
-			}
+			$_SESSION['logged'] = true;
+
+			//associative table so now we can use table column names
+			$user_table = $result->fetch_assoc();
+
+			//place data from associative table to selected variable
+			$_SESSION['id'] = $user_table['id'];
+			$_SESSION['email'] = $user_table['email'];
+
+			unset($_SESSION['error']);
+
+			//clear
+			$result->free_result();
+			header('Location: index.php');
+
+		} else {
+			$_SESSION['error'] = '<span style="color:red">Wrong login or password!</span>';
+			header('Location: index.php');
 		}
 		$connect->close();
 ?>
